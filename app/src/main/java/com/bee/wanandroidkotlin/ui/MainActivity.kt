d68917c3ff0ac041f80ae9fb62fb78943a04406f
zhouxiaojun <zhouxiaojun@gomeplus.com>
2020-03-27 18:06:49 +0800

    首页bottom添加

--- app/src/main/java/com/bee/wanandroidkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/bee/wanandroidkotlin/ui/MainActivity.kt
@@ -3,14 +3,18 @@ package com.bee.wanandroidkotlin.ui
 import android.content.Intent
 import com.bee.baselibrary.base.BaseActivity
 import com.bee.baselibrary.utils.Preference
-import com.bee.wanandroidkotlin.constants.Constants
 import com.bee.wanandroidkotlin.R
+import com.bee.wanandroidkotlin.constants.Constants
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun getContentLayoutId(): Int = R.layout.activity_main
 
     override fun initView() {
         toolBarBuilder.hideBackIcon()
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            true
+        }
     }
 
     override fun initData(intent: Intent?) {
